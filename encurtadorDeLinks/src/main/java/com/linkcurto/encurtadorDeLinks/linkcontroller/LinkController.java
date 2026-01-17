a4821f6549d6822353f21adae70a665971a238fd
Renan <renanlimaq@outlook.com>
2026-01-17 19:12:41 -0300

    Termmiando implementação do redirecionamento

--- encurtadorDeLinks/src/main/java/com/linkcurto/encurtadorDeLinks/linkcontroller/LinkController.java
+++ encurtadorDeLinks/src/main/java/com/linkcurto/encurtadorDeLinks/linkcontroller/LinkController.java
@@ -1,11 +1,13 @@
 package com.linkcurto.encurtadorDeLinks.linkcontroller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.linkcurto.encurtadorDeLinks.linkmodel.LinkModel;
 import com.linkcurto.encurtadorDeLinks.linkservice.LinkService;
+
 
 @RestController
 @RequestMapping("/links")
@@ -31,4 +34,14 @@ public class LinkController {
     public ResponseEntity<List<LinkModel>> getAllLinks(){
         return new ResponseEntity<>(linkService.getAllLinks(), HttpStatus.OK);
     }
+
+    @GetMapping("/{code}")
+    public void redirect(@PathVariable String shortUrl){
+
+        LinkModel link = linkService.getOriginalLink(shortUrl);
+
+        ResponseEntity.status(HttpStatus.FOUND).location(URI.create(link.getOriginalLink()));
+
+    }
+    
 }
